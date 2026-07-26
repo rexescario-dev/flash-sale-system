45e412af03aa93c3c787eba15d52d3eb49093708
Rex Escario, Jr <rex@systematix.co.uk>
2026-07-27 01:14:22 +0800

    feat: add FlashSale domain model package

---
+++ packages/domain/src/index.ts
@@ -0,0 +1,8 @@
+export { FlashSaleValidationError } from './flash-sale/flash-sale.errors.js';
+export type { FlashSaleValidationErrorCode } from './flash-sale/flash-sale.errors.js';
+export { FlashSale } from './flash-sale/flash-sale.js';
+export type {
+  FlashSaleCreateProps,
+  FlashSaleReconstituteProps,
+} from './flash-sale/flash-sale.js';
+export type { FlashSaleId, ProductId } from './ids.js';
