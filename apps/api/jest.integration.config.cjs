5eb5eadc2d9e65419d2ca62072ad3b273cb41cc5
Rex Escario, Jr <rex.escario.jr@gmail.com>
2026-07-28 05:48:49 +0800

    test: add FlashSale repository PostgreSQL integration coverage

---
+++ apps/api/jest.integration.config.cjs
@@ -0,0 +1,18 @@
+/* global module, process */
+process.env.NODE_ENV = 'test';
+
+/** @type {import('jest').Config} */
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@flash-sale/domain$': '<rootDir>/../../packages/domain/src/index.ts',
+  },
+  rootDir: '.',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/test/flash-sale/**/*.spec.ts'],
+  testTimeout: 30_000,
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
