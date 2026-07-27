5bfb88cbccb60663b702ba3cbb718c8ebba98b6b
Rex Escario, Jr <rex.escario.jr@gmail.com>
2026-07-28 05:48:43 +0800

    feat: map Prisma FlashSale rows to domain via reconstitute

--- apps/api/jest.config.cjs
+++ apps/api/jest.config.cjs
@@ -1,14 +1,19 @@
+/* global module, process */
 process.env.NODE_ENV = 'test';
 
 /** @type {import('jest').Config} */
 module.exports = {
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
   moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@flash-sale/domain$': '<rootDir>/../../../packages/domain/src/index.ts',
+  },
   rootDir: 'src',
+  testEnvironment: 'node',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
-  testEnvironment: 'node',
 };
