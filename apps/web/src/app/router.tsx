91f6bd0b0ab9acd5658e25001160fb80a830c132
Rex Escario, Jr <rex.escario.jr@gmail.com>
2026-07-30 20:30:33 +0800

    feat(web): nest customer routes under CustomerLayout shell

--- apps/web/src/app/router.tsx
+++ apps/web/src/app/router.tsx
@@ -4,13 +4,16 @@ import { CatalogPage } from '../pages/CatalogPage';
 import { FlashSalePage } from '../pages/FlashSalePage';
 import { NotFoundPage } from '../pages/NotFoundPage';
 import { PurchasesPage } from '../pages/PurchasesPage';
+import { CustomerLayout } from './CustomerLayout';
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route element={<CatalogPage />} path="/" />
-      <Route element={<FlashSalePage />} path="/sales/:flashSaleId" />
-      <Route element={<PurchasesPage />} path="/purchases" />
+      <Route element={<CustomerLayout />}>
+        <Route element={<CatalogPage />} path="/" />
+        <Route element={<FlashSalePage />} path="/sales/:flashSaleId" />
+        <Route element={<PurchasesPage />} path="/purchases" />
+      </Route>
       <Route element={<NotFoundPage />} path="*" />
     </Routes>
   );
